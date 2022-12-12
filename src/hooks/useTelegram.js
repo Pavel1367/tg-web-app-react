0e1e483134b6b176bcd0f223a6f6825b422e994b
pavel <pashka1367@bk.ru>
2022-12-12 17:24:33 +0300

    T

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -1,22 +1,24 @@
 const tg = window.Telegram.WebApp;
 
-function useTelegram(){
-   
-    const onClose = () =>{
-         tg.close()
-       }
-       const onToggleButton = () =>{
-        if(tg.MainButton.isVisible){
-            tg.MainButton.hide()
-        } else{
-            tg.MainButton.show()
+export function useTelegram() {
+
+    const onClose = () => {
+        tg.close()
+    }
+
+    const onToggleButton = () => {
+        if(tg.MainButton.isVisible) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
         }
-       }
-    return{
-        tg,
+    }
+
+    return {
         onClose,
         onToggleButton,
+        tg,
         user: tg.initDataUnsafe?.user,
+        queryId: tg.initDataUnsafe?.query_id,
     }
 }
-export default useTelegram
